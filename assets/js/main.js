847c5aca788d4211c789037b8ff5d868c439336d
freetimezp <kraftukrnet@ukr.net>
2025-04-15 20:10:15 +0300

    03. usefull-things-123-project-page. GSAP animation.

---
+++ assets/js/main.js
@@ -0,0 +1,271 @@
+const data = [
+    {
+        itemName: "The Frozen",
+        itemCategory: "Nature",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-1.jpg"
+    },
+    {
+        itemName: "Waves of Kiss",
+        itemCategory: "Love",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-2.jpg"
+    },
+    {
+        itemName: "The Wind",
+        itemCategory: "Nature",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-3.jpg"
+    },
+    {
+        itemName: "Fairy in the Emperor",
+        itemCategory: "Cities",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-4.jpg"
+    },
+    {
+        itemName: "Vacant Door",
+        itemCategory: "Cities",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-5.jpg"
+    },
+    {
+        itemName: "The Last Streams",
+        itemCategory: "Nature",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-6.jpg"
+    },
+    {
+        itemName: "Men in the Tale",
+        itemCategory: "Movies",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-7.jpg"
+    },
+    {
+        itemName: "Last Silver",
+        itemCategory: "Factory",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-8.jpg"
+    },
+    {
+        itemName: "The Invisible Healer",
+        itemCategory: "Medicine",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-9.jpg"
+    },
+    {
+        itemName: "Road in the Mist",
+        itemCategory: "Nature",
+        itemLink: "#",
+        itemCopy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, 
+            enim nulla impedit autem architecto quo dolore eaque est aliquam placeat 
+            nesciunt sapiente doloremque nihil rem assumenda quibusdam deleniti facere
+            adipisci tempore corrupti dicta optio, obcaecati officiis. Vel voluptatem 
+            ullam quasi, commodi consequuntur nobis eligendi beatae, quo dolores 
+            quibusdam, animi doloremque voluptatum totam doloribus quas reprehenderit!
+            Illum, eos ad tenetur eligendi, ullam nulla sint quae temporibus delectus, 
+            perspiciatis fugit rem. Maxime eveniet ullam! Ducimus maxime, ad atque ipsa 
+            eum fugit sed tempora vero quam explicabo officiis aut aperiam pariatur et, 
+            suscipit omnis, natus temporibus cum veritatis inventore illo aspernatur 
+            accusantium.
+        `,
+        itemImg: "./assets/images/item-10.jpg"
+    }
+];
+
+const overlay = document.querySelector(".overlay");
+const closeBtn = document.querySelector("#close-btn");
+
+const tl = gsap.timeline({ paused: true, overwrite: "auto" });
+
+tl.to(overlay, {
+    duration: 0.5,
+    bottom: "0px",
+    rotation: 0,
+    transformOrigin: "bottom center",
+    ease: "power3.inOut"
+});
+
+const items = document.querySelectorAll(".item");
+
+items.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        updateOverlay(data[index]);
+
+        tl.play();
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    tl.reverse();
+});
+
+function updateOverlay(dataItem) {
+    const itemName = document.querySelector("#item-name");
+    const itemCategory = document.querySelector("#item-category");
+    const itemLink = document.querySelector("#item-link");
+    const itemCopy = document.querySelector("#item-copy");
+    const itemImg = document.querySelector("#item-img");
+
+    itemName.textContent = dataItem.itemName;
+    itemCategory.textContent = dataItem.itemCategory;
+    itemLink.href = dataItem.itemLink;
+    itemCopy.textContent = dataItem.itemCopy;
+    itemImg.src = dataItem.itemImg;
+}
+
+document.addEventListener("click", (e) => {
+    if (!overlay.contains(e.target) && !isItem(e.target)) {
+        tl.reverse();
+    }
+});
+
+function isItem(target) {
+    return target.closest(".item");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
